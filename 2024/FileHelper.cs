df0a038c1ea11b31213b9b03c8d04ccbb8e3197d
Erik Martin <ewok.attok@gmail.com>
2025-02-11 01:42:47 +0000

    Initial commit

---
+++ 2024/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace _2024
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// Read the specified text file and returns a string array with one line per entry.
+        /// </summary>
+        /// <param name="relativePath">The relative path and file name (starting in the TestFiles folder).</param>
+        /// <returns>A string array with the text file contents.</returns>
+        public static string[] ReadFile(string relativePath)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(relativePath);
+
+            return File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "/TestFiles/" + relativePath);
+        }
+    }
+}
