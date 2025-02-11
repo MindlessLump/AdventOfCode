3dd3b94b4fc4d66d3db17a01b2918422f04b1853
Erik Martin <ewok.attok@gmail.com>
2025-02-11 02:52:57 +0000

    2024: Complete Day 2

--- 2024/Program.cs
+++ 2024/Program.cs
@@ -1,12 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-using _2024;
+﻿using _2024;
 using _2024.Problems;
 
-Console.WriteLine("Advent of Code 2024");
+Console.WriteLine("Advent of Code 2024\n");
 
-Console.WriteLine("_____Day 1_____");
+Console.WriteLine("_____Day 01_____");
 Console.WriteLine(Day1.FindTotalDistance(FileHelper.ReadFile("day1-1.txt"))); // Answer is 11
 Console.WriteLine(Day1.FindTotalDistance(FileHelper.ReadFile("day1-input.txt"))); // Answer for me is 2367773
 Console.WriteLine(Day1.GetSimilarityScore(FileHelper.ReadFile("day1-1.txt"))); // Answer is 31
 Console.WriteLine(Day1.GetSimilarityScore(FileHelper.ReadFile("day1-input.txt"))); // Answer for me is 21271939
+Console.WriteLine("\n\n");
+
+Console.WriteLine("_____Day 02_____");
+Console.WriteLine(Day2.FindSafeReports(FileHelper.ReadFile("day2-1.txt"))); // Answer is 2
+Console.WriteLine(Day2.FindSafeReports(FileHelper.ReadFile("day2-input.txt"))); // Answer for me is 549
+Console.WriteLine(Day2.FindSafeReportsWithDampener(FileHelper.ReadFile("day2-1.txt"))); // Answer is 4
+Console.WriteLine(Day2.FindSafeReportsWithDampener(FileHelper.ReadFile("day2-input.txt"))); // Answer for me is 589
 Console.WriteLine("\n\n");
