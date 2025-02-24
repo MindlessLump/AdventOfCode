55f90e5ef24ed2caa93e2ed4605991f2d586bc8c
Erik Martin <ewok.attok@gmail.com>
2025-02-23 22:33:19 -0700

    2024: Complete Day 14

---
+++ 2024/Problems/Day14.cs
@@ -0,0 +1,255 @@
+﻿using System.Numerics;
+
+namespace _2024.Problems
+{
+    public class Day14
+    {
+        /*
+        To get The Historian safely to the bathroom, you'll need a way to predict where the robots will be in the future. Fortunately, they all seem to be moving on the tile floor in predictable straight lines.
+
+        You make a list (your puzzle input) of all of the robots' current positions (p) and velocities (v), one robot per line. For example:
+
+        p=0,4 v=3,-3
+        p=6,3 v=-1,-3
+        p=10,3 v=-1,2
+        p=2,0 v=2,-1
+        p=0,0 v=1,3
+        p=3,0 v=-2,-2
+        p=7,6 v=-1,-3
+        p=3,0 v=-1,-2
+        p=9,3 v=2,3
+        p=7,3 v=-1,2
+        p=2,4 v=2,-3
+        p=9,5 v=-3,-3
+        Each robot's position is given as p=x,y where x represents the number of tiles the robot is from the left wall and y represents the number of tiles from the top wall (when viewed from above). So, a position of p=0,0 means the robot is all the way in the top-left corner.
+
+        Each robot's velocity is given as v=x,y where x and y are given in tiles per second. Positive x means the robot is moving to the right, and positive y means the robot is moving down. So, a velocity of v=1,-2 means that each second, the robot moves 1 tile to the right and 2 tiles up.
+
+        The robots outside the actual bathroom are in a space which is 101 tiles wide and 103 tiles tall (when viewed from above). However, in this example, the robots are in a space which is only 11 tiles wide and 7 tiles tall.
+
+        The robots are good at navigating over/under each other (due to a combination of springs, extendable legs, and quadcopters), so they can share the same tile and don't interact with each other. Visually, the number of robots on each tile in this example looks like this:
+
+        1.12.......
+        ...........
+        ...........
+        ......11.11
+        1.1........
+        .........1.
+        .......1...
+        These robots have a unique feature for maximum bathroom security: they can teleport. When a robot would run into an edge of the space they're in, they instead teleport to the other side, effectively wrapping around the edges. Here is what robot p=2,4 v=2,-3 does for the first few seconds:
+
+        Initial state:
+        ...........
+        ...........
+        ...........
+        ...........
+        ..1........
+        ...........
+        ...........
+
+        After 1 second:
+        ...........
+        ....1......
+        ...........
+        ...........
+        ...........
+        ...........
+        ...........
+
+        After 2 seconds:
+        ...........
+        ...........
+        ...........
+        ...........
+        ...........
+        ......1....
+        ...........
+
+        After 3 seconds:
+        ...........
+        ...........
+        ........1..
+        ...........
+        ...........
+        ...........
+        ...........
+
+        After 4 seconds:
+        ...........
+        ...........
+        ...........
+        ...........
+        ...........
+        ...........
+        ..........1
+
+        After 5 seconds:
+        ...........
+        ...........
+        ...........
+        .1.........
+        ...........
+        ...........
+        ...........
+        The Historian can't wait much longer, so you don't have to simulate the robots for very long. Where will the robots be after 100 seconds?
+
+        In the above example, the number of robots on each tile after 100 seconds has elapsed looks like this:
+
+        ......2..1.
+        ...........
+        1..........
+        .11........
+        .....1.....
+        ...12......
+        .1....1....
+        To determine the safest area, count the number of robots in each quadrant after 100 seconds. Robots that are exactly in the middle (horizontally or vertically) don't count as being in any quadrant, so the only relevant robots are:
+
+        ..... 2..1.
+        ..... .....
+        1.... .....
+           
+        ..... .....
+        ...12 .....
+        .1... 1....
+        In this example, the quadrants contain 1, 3, 4, and 1 robot. Multiplying these together gives a total safety factor of 12.
+
+        Predict the motion of the robots in your list within a space which is 101 tiles wide and 103 tiles tall. What will the safety factor be after exactly 100 seconds have elapsed?
+         */
+        public static int FindSafetyFactor(string[] file, int seconds, int widthX, int widthY)
+        {
+            // Parse the file to get starting positions and velocities for each robot
+            List<(Vector2, Vector2)> robots = [];
+            foreach (string line in file)
+            {
+                var split = line.Split(' ').Select(s => s.Substring(2)).ToList(); // Remove "p=" and "v="
+                var pos = split[0].Split(',');
+                var vel = split[1].Split(',');
+                robots.Add((new Vector2(int.Parse(pos[0]), int.Parse(pos[1])), new Vector2(int.Parse(vel[0]), int.Parse(vel[1]))));
+            }
+
+            // Track each robot's movement throughout the specified number of seconds
+            for (int j = 0; j < robots.Count; j++)
+            {
+                (var robotPos, var robotVel) = robots[j];
+
+                var newPos = robotPos + robotVel * seconds;
+                while (newPos.X >= widthX)
+                {
+                    newPos.X -= widthX;
+                }
+                while (newPos.X < 0)
+                {
+                    newPos.X += widthX;
+                }
+                while (newPos.Y >= widthY)
+                {
+                    newPos.Y -= widthY;
+                }
+                while (newPos.Y < 0)
+                {
+                    newPos.Y += widthY;
+                }
+
+                robots[j] = (newPos, robotVel);
+            }
+
+            // Count the number of robots in each quadrant
+            double middleX = (double)widthX / 2 - 0.5;
+            double middleY = (double)widthY / 2 - 0.5;
+            int quadrant1 = 0;
+            int quadrant2 = 0;
+            int quadrant3 = 0;
+            int quadrant4 = 0;
+            foreach ((var pos, var vel) in robots)
+            {
+                if (pos.X > middleX && pos.Y < middleY)
+                {
+                    quadrant1++;
+                }
+                else if (pos.X > middleX && pos.Y > middleY)
+                {
+                    quadrant2++;
+                }
+                else if (pos.X < middleX && pos.Y > middleY)
+                {
+                    quadrant3++;
+                }
+                else if (pos.X < middleX && pos.Y < middleY)
+                {
+                    quadrant4++;
+                }
+            }
+
+            return quadrant1 * quadrant2 * quadrant3 * quadrant4;
+        }
+
+        /*
+        During the bathroom break, someone notices that these robots seem awfully similar to ones built and used at the North Pole. If they're the same type of robots, they should have a hard-coded Easter egg: very rarely, most of the robots should arrange themselves into a picture of a Christmas tree.
+
+        What is the fewest number of seconds that must elapse for the robots to display the Easter egg?
+         */
+        public static int FindChristmasTree(string[] file, int widthX, int widthY)
+        {
+            // Parse the file to get starting positions and velocities for each robot
+            List<(Vector2, Vector2)> robots = [];
+            int[,] map = new int[widthY, widthX];
+            for (int y = 0; y < widthY; y++)
+            {
+                for (int x = 0; x < widthX; x++)
+                {
+                    map[y, x] = 0;
+                }
+            }
+
+            foreach (string line in file)
+            {
+                var split = line.Split(' ').Select(s => s.Substring(2)).ToList(); // Remove "p=" and "v="
+                var pos = split[0].Split(',').Select(s => int.Parse(s)).ToList();
+                var vel = split[1].Split(',').Select(s => int.Parse(s)).ToList();
+                robots.Add((new Vector2(pos[0], pos[1]), new Vector2(vel[0], vel[1])));
+
+                map[pos[1], pos[0]]++;
+            }
+
+            // Track each robot's movement throughout the specified number of seconds
+            int maxSteps = widthX * widthY;
+            var middle = new Vector2((float)((double)widthX / 2 - 0.5), (float)((double)widthY / 2 - 0.5));
+            Dictionary<int, double> spreadByStep = [];
+            for (int step = 1; step <= maxSteps; step++)
+            {
+                double totalDist = 0;
+                for (int j = 0; j < robots.Count; j++)
+                {
+                    (var robotPos, var robotVel) = robots[j];
+
+                    var newPos = robotPos + robotVel;
+                    while (newPos.X >= widthX)
+                    {
+                        newPos.X -= widthX;
+                    }
+                    while (newPos.X < 0)
+                    {
+                        newPos.X += widthX;
+                    }
+                    while (newPos.Y >= widthY)
+                    {
+                        newPos.Y -= widthY;
+                    }
+                    while (newPos.Y < 0)
+                    {
+                        newPos.Y += widthY;
+                    }
+
+                    map[(int)robotPos.Y, (int)robotPos.X]--;
+                    map[(int)newPos.Y, (int)newPos.X]++;
+                    robots[j] = (newPos, robotVel);
+                    totalDist += (middle - newPos).Length();
+                }
+
+                spreadByStep.Add(step, totalDist);
+            }
+
+            return spreadByStep.OrderBy(kvp => kvp.Value).First().Key;
+        }
+    }
+}
--- 2024/Program.cs
+++ 2024/Program.cs
@@ -3,79 +3,84 @@ using _2024.Problems;
 
 Console.WriteLine("Advent of Code 2024");
 
-Console.WriteLine("\n_____Day 01_____");
-Console.WriteLine(Day01.FindTotalDistance(FileHelper.ReadLines("day01-1.txt"))); // Answer is 11
-Console.WriteLine(Day01.FindTotalDistance(FileHelper.ReadLines("day01-input.txt"))); // Answer for me is 2367773
-Console.WriteLine(Day01.GetSimilarityScore(FileHelper.ReadLines("day01-1.txt"))); // Answer is 31
-Console.WriteLine(Day01.GetSimilarityScore(FileHelper.ReadLines("day01-input.txt"))); // Answer for me is 21271939
+//Console.WriteLine("\n_____Day 01_____");
+//Console.WriteLine(Day01.FindTotalDistance(FileHelper.ReadLines("day01-1.txt"))); // Answer is 11
+//Console.WriteLine(Day01.FindTotalDistance(FileHelper.ReadLines("day01-input.txt"))); // Answer for me is 2367773
+//Console.WriteLine(Day01.GetSimilarityScore(FileHelper.ReadLines("day01-1.txt"))); // Answer is 31
+//Console.WriteLine(Day01.GetSimilarityScore(FileHelper.ReadLines("day01-input.txt"))); // Answer for me is 21271939
 
-Console.WriteLine("\n_____Day 02_____");
-Console.WriteLine(Day02.FindSafeReports(FileHelper.ReadLines("day02-1.txt"))); // Answer is 2
-Console.WriteLine(Day02.FindSafeReports(FileHelper.ReadLines("day02-input.txt"))); // Answer for me is 549
-Console.WriteLine(Day02.FindSafeReportsWithDampener(FileHelper.ReadLines("day02-1.txt"))); // Answer is 4
-Console.WriteLine(Day02.FindSafeReportsWithDampener(FileHelper.ReadLines("day02-input.txt"))); // Answer for me is 589
+//Console.WriteLine("\n_____Day 02_____");
+//Console.WriteLine(Day02.FindSafeReports(FileHelper.ReadLines("day02-1.txt"))); // Answer is 2
+//Console.WriteLine(Day02.FindSafeReports(FileHelper.ReadLines("day02-input.txt"))); // Answer for me is 549
+//Console.WriteLine(Day02.FindSafeReportsWithDampener(FileHelper.ReadLines("day02-1.txt"))); // Answer is 4
+//Console.WriteLine(Day02.FindSafeReportsWithDampener(FileHelper.ReadLines("day02-input.txt"))); // Answer for me is 589
 
-Console.WriteLine("\n_____Day 03_____");
-Console.WriteLine(Day03.TotalMul(FileHelper.ReadWholeFile("day03-1.txt"))); // Answer is 161
-Console.WriteLine(Day03.TotalMul(FileHelper.ReadWholeFile("day03-input.txt"))); // Answer for me is 175615763
-Console.WriteLine(Day03.ToggledMul(FileHelper.ReadWholeFile("day03-2.txt"))); // Answer is 48
-Console.WriteLine(Day03.ToggledMul(FileHelper.ReadWholeFile("day03-input.txt"))); // Answer for me is 74361272
+//Console.WriteLine("\n_____Day 03_____");
+//Console.WriteLine(Day03.TotalMul(FileHelper.ReadWholeFile("day03-1.txt"))); // Answer is 161
+//Console.WriteLine(Day03.TotalMul(FileHelper.ReadWholeFile("day03-input.txt"))); // Answer for me is 175615763
+//Console.WriteLine(Day03.ToggledMul(FileHelper.ReadWholeFile("day03-2.txt"))); // Answer is 48
+//Console.WriteLine(Day03.ToggledMul(FileHelper.ReadWholeFile("day03-input.txt"))); // Answer for me is 74361272
 
-Console.WriteLine("\n_____Day 04_____");
-Console.WriteLine(Day04.WordSearch(FileHelper.ReadLines("day04-1.txt"))); // Answer is 18
-Console.WriteLine(Day04.WordSearch(FileHelper.ReadLines("day04-input.txt"))); // Answer for me is 2434
-Console.WriteLine(Day04.CrossSearch(FileHelper.ReadLines("day04-1.txt"))); // Answer is 9
-Console.WriteLine(Day04.CrossSearch(FileHelper.ReadLines("day04-input.txt"))); // Answer for me is 1835
+//Console.WriteLine("\n_____Day 04_____");
+//Console.WriteLine(Day04.WordSearch(FileHelper.ReadLines("day04-1.txt"))); // Answer is 18
+//Console.WriteLine(Day04.WordSearch(FileHelper.ReadLines("day04-input.txt"))); // Answer for me is 2434
+//Console.WriteLine(Day04.CrossSearch(FileHelper.ReadLines("day04-1.txt"))); // Answer is 9
+//Console.WriteLine(Day04.CrossSearch(FileHelper.ReadLines("day04-input.txt"))); // Answer for me is 1835
 
-Console.WriteLine("\n_____Day 05_____");
-Console.WriteLine(Day05.CheckPageOrder(FileHelper.ReadLines("day05-1.txt"))); // Answer is 143
-Console.WriteLine(Day05.CheckPageOrder(FileHelper.ReadLines("day05-input.txt"))); // Answer for me is 7198
-Console.WriteLine(Day05.ReorderPageOrder(FileHelper.ReadLines("day05-1.txt"))); // Answer is 123
-Console.WriteLine(Day05.ReorderPageOrder(FileHelper.ReadLines("day05-input.txt"))); // Answer for me is 4230
+//Console.WriteLine("\n_____Day 05_____");
+//Console.WriteLine(Day05.CheckPageOrder(FileHelper.ReadLines("day05-1.txt"))); // Answer is 143
+//Console.WriteLine(Day05.CheckPageOrder(FileHelper.ReadLines("day05-input.txt"))); // Answer for me is 7198
+//Console.WriteLine(Day05.ReorderPageOrder(FileHelper.ReadLines("day05-1.txt"))); // Answer is 123
+//Console.WriteLine(Day05.ReorderPageOrder(FileHelper.ReadLines("day05-input.txt"))); // Answer for me is 4230
 
-Console.WriteLine("\n_____Day 06_____");
-Console.WriteLine(Day06.CountDistinctPositions(FileHelper.ReadLines("day06-1.txt"))); // Answer is 41
-Console.WriteLine(Day06.CountDistinctPositions(FileHelper.ReadLines("day06-input.txt"))); // Answer for me is 4647
-Console.WriteLine(Day06.CountPossibleLoops(FileHelper.ReadLines("day06-1.txt"))); // Answer is 6
-Console.WriteLine(Day06.CountPossibleLoops(FileHelper.ReadLines("day06-input.txt"))); // Answer for me is 1723
+//Console.WriteLine("\n_____Day 06_____");
+//Console.WriteLine(Day06.CountDistinctPositions(FileHelper.ReadLines("day06-1.txt"))); // Answer is 41
+//Console.WriteLine(Day06.CountDistinctPositions(FileHelper.ReadLines("day06-input.txt"))); // Answer for me is 4647
+//Console.WriteLine(Day06.CountPossibleLoops(FileHelper.ReadLines("day06-1.txt"))); // Answer is 6
+//Console.WriteLine(Day06.CountPossibleLoops(FileHelper.ReadLines("day06-input.txt"))); // Answer for me is 1723
 
-Console.WriteLine("\n_____Day 07_____");
-Console.WriteLine(Day07.FindTrueEquations(FileHelper.ReadLines("day07-1.txt"))); // Answer is 3749
-Console.WriteLine(Day07.FindTrueEquations(FileHelper.ReadLines("day07-input.txt"))); // Answer for me is 2314935962622
-Console.WriteLine(Day07.FindMoreTrueEquations(FileHelper.ReadLines("day07-1.txt"))); // Answer is 11387
-Console.WriteLine(Day07.FindMoreTrueEquations(FileHelper.ReadLines("day07-input.txt"))); // Answer for me is 401477450831495
+//Console.WriteLine("\n_____Day 07_____");
+//Console.WriteLine(Day07.FindTrueEquations(FileHelper.ReadLines("day07-1.txt"))); // Answer is 3749
+//Console.WriteLine(Day07.FindTrueEquations(FileHelper.ReadLines("day07-input.txt"))); // Answer for me is 2314935962622
+//Console.WriteLine(Day07.FindMoreTrueEquations(FileHelper.ReadLines("day07-1.txt"))); // Answer is 11387
+//Console.WriteLine(Day07.FindMoreTrueEquations(FileHelper.ReadLines("day07-input.txt"))); // Answer for me is 401477450831495
 
-Console.WriteLine("\n_____Day 08_____");
-Console.WriteLine(Day08.FindAntinodes(FileHelper.ReadLines("day08-1.txt"))); // Answer is 14
-Console.WriteLine(Day08.FindAntinodes(FileHelper.ReadLines("day08-input.txt"))); // Answer for me is 228
-Console.WriteLine(Day08.FindHarmonicAntinodes(FileHelper.ReadLines("day08-1.txt"))); // Answer is 34
-Console.WriteLine(Day08.FindHarmonicAntinodes(FileHelper.ReadLines("day08-input.txt"))); // Answer for me is 766
+//Console.WriteLine("\n_____Day 08_____");
+//Console.WriteLine(Day08.FindAntinodes(FileHelper.ReadLines("day08-1.txt"))); // Answer is 14
+//Console.WriteLine(Day08.FindAntinodes(FileHelper.ReadLines("day08-input.txt"))); // Answer for me is 228
+//Console.WriteLine(Day08.FindHarmonicAntinodes(FileHelper.ReadLines("day08-1.txt"))); // Answer is 34
+//Console.WriteLine(Day08.FindHarmonicAntinodes(FileHelper.ReadLines("day08-input.txt"))); // Answer for me is 766
 
-Console.WriteLine("\n_____Day 09_____");
-Console.WriteLine(Day09.FindCompressedChecksum(FileHelper.ReadWholeFile("day09-1.txt"))); // Answer is 1928
-Console.WriteLine(Day09.FindCompressedChecksum(FileHelper.ReadWholeFile("day09-input.txt"))); // Answer for me is 6330095022244
-Console.WriteLine(Day09.FindChecksumWithoutFragmentation(FileHelper.ReadWholeFile("day09-1.txt"))); // Answer is 2858
-Console.WriteLine(Day09.FindChecksumWithoutFragmentation(FileHelper.ReadWholeFile("day09-input.txt"))); // Answer for me is 6359491814941
+//Console.WriteLine("\n_____Day 09_____");
+//Console.WriteLine(Day09.FindCompressedChecksum(FileHelper.ReadWholeFile("day09-1.txt"))); // Answer is 1928
+//Console.WriteLine(Day09.FindCompressedChecksum(FileHelper.ReadWholeFile("day09-input.txt"))); // Answer for me is 6330095022244
+//Console.WriteLine(Day09.FindChecksumWithoutFragmentation(FileHelper.ReadWholeFile("day09-1.txt"))); // Answer is 2858
+//Console.WriteLine(Day09.FindChecksumWithoutFragmentation(FileHelper.ReadWholeFile("day09-input.txt"))); // Answer for me is 6359491814941
 
-Console.WriteLine("\n_____Day 10_____");
-Console.WriteLine(Day10.FindTrailheadScores(FileHelper.ReadLines("day10-1.txt"))); // Answer is 1
-Console.WriteLine(Day10.FindTrailheadScores(FileHelper.ReadLines("day10-2.txt"))); // Answer is 36
-Console.WriteLine(Day10.FindTrailheadScores(FileHelper.ReadLines("day10-input.txt"))); // Answer for me is 566
-Console.WriteLine(Day10.FindTrailheadRatings(FileHelper.ReadLines("day10-2.txt"))); // Answer is 81
-Console.WriteLine(Day10.FindTrailheadRatings(FileHelper.ReadLines("day10-input.txt"))); // Answer for me is 1324
+//Console.WriteLine("\n_____Day 10_____");
+//Console.WriteLine(Day10.FindTrailheadScores(FileHelper.ReadLines("day10-1.txt"))); // Answer is 1
+//Console.WriteLine(Day10.FindTrailheadScores(FileHelper.ReadLines("day10-2.txt"))); // Answer is 36
+//Console.WriteLine(Day10.FindTrailheadScores(FileHelper.ReadLines("day10-input.txt"))); // Answer for me is 566
+//Console.WriteLine(Day10.FindTrailheadRatings(FileHelper.ReadLines("day10-2.txt"))); // Answer is 81
+//Console.WriteLine(Day10.FindTrailheadRatings(FileHelper.ReadLines("day10-input.txt"))); // Answer for me is 1324
 
-Console.WriteLine("\n_____Day 11_____");
-Console.WriteLine(Day11.CountStonesAfterBlinks(FileHelper.ReadWholeFile("day11-1.txt"), 25)); // Answer is 55312
-Console.WriteLine(Day11.CountStonesAfterBlinks(FileHelper.ReadWholeFile("day11-input.txt"), 25)); // Answer for me is 200446
-Console.WriteLine(Day11.CountStonesAfterBlinks(FileHelper.ReadWholeFile("day11-input.txt"), 75)); // Answer for me is 238317474993392
+//Console.WriteLine("\n_____Day 11_____");
+//Console.WriteLine(Day11.CountStonesAfterBlinks(FileHelper.ReadWholeFile("day11-1.txt"), 25)); // Answer is 55312
+//Console.WriteLine(Day11.CountStonesAfterBlinks(FileHelper.ReadWholeFile("day11-input.txt"), 25)); // Answer for me is 200446
+//Console.WriteLine(Day11.CountStonesAfterBlinks(FileHelper.ReadWholeFile("day11-input.txt"), 75)); // Answer for me is 238317474993392
 
-Console.WriteLine("\n_____Day 12_____");
-Console.WriteLine(Day12.CalculateTotalFenceCosts(FileHelper.ReadLines("day12-1.txt"))); // Answer is 1930
-Console.WriteLine(Day12.CalculateTotalFenceCosts(FileHelper.ReadLines("day12-input.txt"))); // Answer for me is 1477924
-Console.WriteLine(Day12.CalculateDiscountedFenceCosts(FileHelper.ReadLines("day12-1.txt"))); // Answer is 1206
-Console.WriteLine(Day12.CalculateDiscountedFenceCosts(FileHelper.ReadLines("day12-input.txt"))); // Answer for me is 841934
+//Console.WriteLine("\n_____Day 12_____");
+//Console.WriteLine(Day12.CalculateTotalFenceCosts(FileHelper.ReadLines("day12-1.txt"))); // Answer is 1930
+//Console.WriteLine(Day12.CalculateTotalFenceCosts(FileHelper.ReadLines("day12-input.txt"))); // Answer for me is 1477924
+//Console.WriteLine(Day12.CalculateDiscountedFenceCosts(FileHelper.ReadLines("day12-1.txt"))); // Answer is 1206
+//Console.WriteLine(Day12.CalculateDiscountedFenceCosts(FileHelper.ReadLines("day12-input.txt"))); // Answer for me is 841934
 
-Console.WriteLine("\n_____Day 13_____");
-Console.WriteLine(Day13.CalculateTokensForPrizes(FileHelper.ReadLines("day13-1.txt"))); // Answer is 480
-Console.WriteLine(Day13.CalculateTokensForPrizes(FileHelper.ReadLines("day13-input.txt"))); // Answer for me is 26599
-Console.WriteLine(Day13.CalculateTokensForFartherPrizes(FileHelper.ReadLines("day13-input.txt"))); // Answer for me is 106228669504887
+//Console.WriteLine("\n_____Day 13_____");
+//Console.WriteLine(Day13.CalculateTokensForPrizes(FileHelper.ReadLines("day13-1.txt"))); // Answer is 480
+//Console.WriteLine(Day13.CalculateTokensForPrizes(FileHelper.ReadLines("day13-input.txt"))); // Answer for me is 26599
+//Console.WriteLine(Day13.CalculateTokensForFartherPrizes(FileHelper.ReadLines("day13-input.txt"))); // Answer for me is 106228669504887
+
+Console.WriteLine("\n_____Day 14_____");
+Console.WriteLine(Day14.FindSafetyFactor(FileHelper.ReadLines("day14-1.txt"), 100, 11, 7)); // Answer is 12
+Console.WriteLine(Day14.FindSafetyFactor(FileHelper.ReadLines("day14-input.txt"), 100, 101, 103)); // Answer for me is 218295000
+Console.WriteLine(Day14.FindChristmasTree(FileHelper.ReadLines("day14-input.txt"), 101, 103)); // Answer for me is 6870
